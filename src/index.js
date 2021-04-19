364acba5dde498ec3a547cbb692df70750918c83
Jason Christian H <nakamarujc@gmail.com>
2021-04-19 17:28:28 +0700

    Added dotenv library and added https support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// Load environment variables
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
-const { PORT } = require('./env');
+const fs = require('fs');
 const onedchess = require('./engine/chessengine');
 
 // Create the express app
@@ -12,8 +15,19 @@ const publicDir = path.join(__dirname, './../public');
 // Serve static files
 app.use(express.static(publicDir));
 
+// Load certificates, if exists
+const sslFileAvailable = process.env.KEY && process.env.CERT;
+const serverOpt = sslFileAvailable ? {
+    key: fs.readFileSync(process.env.KEY),
+    cert: fs.readFileSync(process.env.CERT)
+} : undefined;
+
+// Load either HTTP / HTTPS module
+const serverModule = sslFileAvailable == undefined ? require('http') : require('https');
+
 // Create HTTP server and listen
-const server = require('http').createServer(app).listen(PORT);
+const port = process.env.PORT || 8080;
+const server = serverModule.createServer(serverOpt, app).listen(port, () => {console.log(`Listening to ${port}`)});
 
 // Listen to WS connections
 const io = require('socket.io')().listen(server);
